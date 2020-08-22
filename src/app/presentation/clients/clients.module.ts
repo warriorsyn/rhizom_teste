b0c424de53b54576abbf3195a23d85930bcc5d58
Joao Victor de Andrade <joao.victor@primesquad.com.br>
2020-08-21 22:49:46 -0300

    Adding client form component

--- src/app/presentation/clients/clients.module.ts
+++ src/app/presentation/clients/clients.module.ts
@@ -5,9 +5,10 @@ import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientListComponent } from './client-list/client-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { ClientCreateComponent } from './client-create/client-create.component';
+import { ClientFormComponent } from './client-form/client-form.component';
 
 @NgModule({
-  declarations: [ClientsComponent, ClientListComponent, ClientCreateComponent],
+  declarations: [ClientsComponent, ClientListComponent, ClientCreateComponent, ClientFormComponent],
   imports: [CommonModule, ClientsRoutingModule, SharedModule],
 })
 export class ClientsModule {}
