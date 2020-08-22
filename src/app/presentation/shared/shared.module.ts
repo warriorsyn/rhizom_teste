1357c9ba652b4c5589e73e897c7cec2ebfaef394
Joao Victor de Andrade <joao.victor@primesquad.com.br>
2020-08-22 11:37:13 -0300

    Adding validation feedback to form client

--- src/app/presentation/shared/shared.module.ts
+++ src/app/presentation/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { ToolbarComponent } from './components/toolbar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
-  declarations: [ToolbarComponent, ButtonComponent, HeaderComponent],
+  declarations: [
+    ToolbarComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FeedbackComponent,
+  ],
   imports: [CommonModule, MatToolbarModule],
-  exports: [ToolbarComponent, ButtonComponent, HeaderComponent],
+  exports: [
+    ToolbarComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FeedbackComponent,
+  ],
 })
 export class SharedModule {}
