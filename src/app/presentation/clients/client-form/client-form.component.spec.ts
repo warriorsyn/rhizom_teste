a378f9ccfb21bd0bdbd988632a237bca62f49404
Joao Victor de Andrade <joao.victor@primesquad.com.br>
2020-08-27 02:46:46 -0300

    Unit test emit form

--- src/app/presentation/clients/client-form/client-form.component.spec.ts
+++ src/app/presentation/clients/client-form/client-form.component.spec.ts
@@ -12,10 +12,14 @@ import { ClientMockRepository } from 'src/app/data/client/client-mock-repository
 import { CepWebRepository } from 'src/app/data/cep/cep-web.repository';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ClientFormComponent', () => {
   let component: ClientFormComponent;
   let fixture: ComponentFixture<ClientFormComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -33,11 +37,21 @@ describe('ClientFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ClientFormComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('form'));
+    el = de.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit form when onSubmit have been called', () => {
+    fixture.detectChanges();
+    spyOn(component, 'emitSubmit');
+    el = fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+    expect(component.emitSubmit).toHaveBeenCalledTimes(1);
   });
 
   it('form should be invalid', () => {
@@ -49,5 +63,17 @@ describe('ClientFormComponent', () => {
     component.clientForm.controls.brand.setValue('');
     component.clientForm.controls.model.setValue('');
     expect(component.clientForm.valid).toBeFalse();
+  });
+
+  it('form should be valid', () => {
+    component.clientForm.controls.name.setValue('Jhson');
+    component.clientForm.controls.cpf.setValue('408.666.490-93');
+    component.clientForm.controls.phone.setValue('(79) 9 9154-1139');
+    component.clientForm.controls.birth.setValue('11/06/2001');
+    component.clientForm.controls.address.setValue('Av Anisio Azevedo');
+    component.clientForm.controls.brand.setValue({ id: 1, name: 'Brand' });
+    component.clientForm.controls.model.setValue({ id: 1, name: 'model' });
+
+    expect(component.clientForm.valid).toBeTrue();
   });
 });
